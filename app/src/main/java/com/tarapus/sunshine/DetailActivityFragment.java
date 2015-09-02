d999f6b6f79325cc293ed58ba2a5bd3227d98376
Andrey <andrey198318@gmail.com>
2015-09-02 14:51:50 -0500

    Display content in DetailActivity

--- app/src/main/java/com/tarapus/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/tarapus/sunshine/DetailActivityFragment.java
@@ -1,14 +1,13 @@
 package com.tarapus.sunshine;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class DetailActivityFragment extends Fragment {
 
     public DetailActivityFragment() {
@@ -17,6 +16,16 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent = getActivity().getIntent();
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+            ((TextView) rootView.findViewById(R.id.detail_text))
+                    .setText(forecastStr);
+        }
+
+        return rootView;
     }
 }
